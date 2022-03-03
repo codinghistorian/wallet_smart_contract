425f29bb9f4e1fe64f9918e21d1c0168ec79ef72
codinghistorian <gaudihaus@gmail.com>
2022-03-03 19:37:25 +0900

    220303 19:37 documentation on solidity code

--- contracts/2_Wallet_Draft.sol
+++ contracts/2_Wallet_Draft.sol
@@ -8,26 +8,23 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 
+// owner here will be the service provider
+// 220303 19:09 KST I should probably make a different role for users
+
 contract Wallet is Ownable, AccessControl {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
-
+    bytes32 public constant USER_ROLE = keccak256("USER_ROLE");
     //@Dev deployer of this contract will be the Owner and fee taker of this contract.
 
     address serviceProvider;
 
-    //@Dev access control by Role added in order to make sure this wallet is used by the right
-    //person
-    //@Param user - address of this wallet contract needed
-    bytes32 public constant USER_ROLE = keccak256("USER_ROLE");
     constructor(address user) {
         serviceProvider=msg.sender;
         _setupRole(USER_ROLE, user);
     }
-
  
     //@Dev Owner of this contract and change fee
-    //@Param newFee - fee in wei
     uint fee = 100000;
 
     event ChangedFee(uint newFee);
@@ -48,10 +45,8 @@ contract Wallet is Ownable, AccessControl {
         emit Deposit(msg.sender, msg.value);
     }
 
-    //@Dev send ETH
+    // send ETH
     event WithdrawETH(address indexed receiver, uint amount);
-    //@Param _to - receiver of ETH
-    // _valueInWei - how much to send)
     function sendETH(address _to, uint _valueInWei) external returns(bool) {
         require(hasRole(USER_ROLE, msg.sender), "Caller is not our user");
         require(address(this).balance >= _valueInWei, "balance ain't enough");
@@ -63,10 +58,8 @@ contract Wallet is Ownable, AccessControl {
         emit WithdrawETH(_to, rest);
     }
 
-
-    // @Dev safeTransfer used for security issue.
-    // @Param IERC20 _token - token that this wallet's user want to send.
-    // _to - receiver's address   _amount - how much to send
+    //event that emits when token is sent
+    //I think I need safetransfer
     event WithdrawERC20(address indexed receiver, uint amount);
 
     function sendToken(IERC20 _token, address _to, uint _amount) external {
@@ -76,13 +69,16 @@ contract Wallet is Ownable, AccessControl {
         emit WithdrawERC20(_to, _amount);
     }
         
+//  * Ability to send/receive tokens
+// -> gotta USE IERC20, SafeERC20 to interact with tokens
+// -> just use safeTransfer lib from openzeppelin.
 
-    // @Dev Please use this function for setting initial value of allowance, not for increasing
-    // or decreasing the allowance value
-    // @Param IERC20 _token - token that this wallet's user want to approve.
-    // _spender - smart contract that will manipulate this contract's token
-    // _amount - how much to allow
+//  * Ability to make allowance for tokens
+// -> gotta inherit IERC20 to this wallet.
+// -> I have never tried making a smart contract calling allowance function, gotta try.
 
+// @Dev Please use this function for setting initial value of allowance, not for increasing
+// or decreasing the allowance value
     event Allowance(address indexed spender, uint value);
     function approve(IERC20 _token, address _spender, uint _amount) external {
         require(hasRole(USER_ROLE, msg.sender), "Caller is not our user");
@@ -92,13 +88,6 @@ contract Wallet is Ownable, AccessControl {
         emit Allowance(_spender, _amount);
     }
 
-    // @Dev Please use this function for increasing the allowance value
-    // If you want to check current allowance of a token, please talk to the ERC20 token
-    // that you are trying to increase allowance of 
-    // @Param IERC20 _token - token that this wallet's user want to approve.
-    // _spender - smart contract that will manipulate this contract's token
-    // _amount - how much to allow
-
     function allowanceIncrease(IERC20 _token, address _spender, uint _amount) external {
         require(hasRole(USER_ROLE, msg.sender), "Caller is not our user");
         require(_token.balanceOf(address(this)) >= _amount, "Not enough tokens");
@@ -106,18 +95,24 @@ contract Wallet is Ownable, AccessControl {
         emit Allowance(_spender, _amount);
     }
 
-    // @Dev Please use this function for increasing the allowance value
-    // If you want to check current allowance of a token, please talk to the ERC20 token
-    // that you are trying to increase allowance of 
-    // @Param IERC20 _token - token that this wallet's user want to approve.
-    // _spender - smart contract that will manipulate this contract's token
-    // _amount - how much to allow    
-
     function allowanceDecrease(IERC20 _token, address _spender, uint _amount) external {
         require(hasRole(USER_ROLE, msg.sender), "Caller is not our user");
         require(_token.balanceOf(address(this)) >= _amount, "Not enough tokens");
         _token.safeDecreaseAllowance(_spender, _amount);
         emit Allowance(_spender, _amount);
     }
+
+//  * There must be a method that changes the value of this fee
+// -> I can just make the fee state variable with  a function that changes it. Onlyowner needed.
+
+//  * Commission - is a certain number of the transferred amount
+// -> for this I need to look at the original text from Russian... so do I need to
+// make the fee absolute? or relative? I don't know yet.
+
+//  * The transfer of this commission must be made along with the regular transfer
+// I can just make this low-level call work twice. once for actual transfer, the other for comission.
+
+//  * The address for the transfer is hardcoded into the contract
+// -> okay. receiver addresse hardcoded.
 
 }
